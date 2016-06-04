eb31811eb9bed53408dde28348b9f079ead8a365
PrimePrince <mojibbz@gmail.com>
2016-06-04 03:35:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main()
 {
-  int i;
-  for (i = 0; i < 32; i++)
-  {
-    printf(“Hello Holberton\n”);
-  }
-  return 0;
+	int i;
+	for (i=0; i<32; i++)
+	{
+		printf ("Hello Holberton\n");
+	}
+   
+   return 0;
 }
